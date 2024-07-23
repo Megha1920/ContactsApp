41aa57d94bfcbbd10f229238f176eba1419be76c
Megha <meghacthomas@gmail.com>
2024-07-23 15:37:01 +0530

    apps admin register

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+# accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+    # Define list_display with fields that are part of the CustomUser model
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    # Define list_filter with fields that are part of the CustomUser model
+    list_filter = ('is_staff', 'is_active')
+    # Remove groups and user_permissions from filter_horizontal
+    filter_horizontal = ()
+    # Define fieldsets to organize the form in the admin
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'gender', 'phone_numbers', 'address', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    # Define add_fieldsets to organize the form for adding users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'gender', 'phone_numbers', 'address', 'profile_picture', 'password1', 'password2'),
+        }),
+    )
+    # Define search_fields to enable search functionality
+    search_fields = ('username', 'email')
+    # Define ordering of the users
+    ordering = ('username',)
+
+admin.site.register(CustomUser, UserAdmin)
+
--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'address', 'company')
+    search_fields = ('first_name', 'last_name', 'address', 'company')
+    list_filter = ('company',)  # Add filters based on company
+    ordering = ('last_name', 'first_name')  # Default ordering
+
+    # Optional: Customize the form for better management of phone numbers
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        # Customize form fields or widgets if needed
+        return form
